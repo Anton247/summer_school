e0198df9f3f9330422cd7460ddc536dc84043125
Anton <kondratov@mail.ru>
2021-07-13 12:00:59 +0700

    Проблема с компиляцией нескольких файлов, но сейчас это не существенно

--- 2/mpi/main.cpp
+++ 2/mpi/main.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+
 void c_print(int n) {
     cout << n;
 }
@@ -22,14 +23,14 @@ int main(int argc, char** argv) {
     int num;
     if (!rank) {
         DF x;
-        //c_init(7, x);
-       // assert(x.get_type() == TYPE_INT);
-        //num = x.getValue<int>();
-        //MPI_Send(&num, 1, MPI_INT, commSize - 1, 0, MPI_COMM_WORLD);
+        c_init(7, x);
+        assert(x.get_type() == TYPE_INT);
+        num = x.getValue<int>();
+        MPI_Send(&num, 1, MPI_INT, commSize - 1, 0, MPI_COMM_WORLD);
     }
     if (rank == commSize - 1) {
-        //MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        //c_print(num);
+        MPI_Recv(&num, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        c_print(num);
     }
     MPI_Finalize();
     return 0;
